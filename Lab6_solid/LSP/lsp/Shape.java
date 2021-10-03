51068aa53ffe2e95fd4a4906773783f08b26807b
phlpat <57593026+phlpat@users.noreply.github.com>
2021-10-04 00:40:11 +0700

    Add files via upload

---
+++ Lab6_solid/LSP/lsp/Shape.java
@@ -0,0 +1,4 @@
+package lsp;
+public abstract class Shape {
+    public abstract int getArea();
+}
---
+++ Lab6_solid/LSP/lsp/Square.java
@@ -0,0 +1,22 @@
+package lsp;
+
+public class Square extends Shape {
+
+    private int side;
+
+    Square(int side) {
+        this.side = side;
+    }
+
+    public int getSide() {
+        return side;
+    }
+
+    public void setSide(int side) {
+        this.side = side;
+    }
+
+    public int getArea() {
+        return side * side;
+    }
+}
